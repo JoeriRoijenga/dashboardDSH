4408bc2817a740b5abd6ea6242b1ebdddeeeeb45
joeriroijenga <roijenga.joeri@gmail.com>
2020-11-25 16:28:30 +0100

    added checks for form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SectionsModule } from './modules/sections/sections.module';
+import { UserModule } from './modules/user/user.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SectionsModule } from './modules/sections/sections.module';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        SectionsModule
+        SectionsModule,
+        UserModule
     ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/user/login/login.component.ts
+++ src/app/modules/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  invalidLogin: boolean;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService
+  ) {
+    this.invalidLogin = false;
+  }
+
+  loginForm = this.formBuilder.group({
+    username: ['', Validators.compose([Validators.required])],
+    password: ['', Validators.required]
+  });
 
   ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    // console.log(this.loginForm.value);
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const data = {
+      username: this.loginForm.controls.username.value,
+      password: this.loginForm.controls.password.value,
+    };
+
+    console.log(data);
+
+    this.invalidLogin = true;
   }
 
 }
