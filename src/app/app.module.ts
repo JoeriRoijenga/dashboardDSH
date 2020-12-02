b20746382674a9984bd75631ce1972c13e47605b
joeriroijenga <roijenga.joeri@gmail.com>
2020-12-02 13:03:04 +0100

    comma fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SectionsModule } from './modules/sections/sections.module';
+import { UserModule } from './modules/user/user.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,7 @@ import { SectionsModule } from './modules/sections/sections.module';
         AppRoutingModule,
         SectionsModule,
         UserModule,
-        HttpClientModule
+        HttpClientModule,
         SectionsModule,
         BrowserAnimationsModule,
         MatDialogModule
