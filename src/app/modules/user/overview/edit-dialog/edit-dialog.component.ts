e22a9b68f6f610befa9b03669411b3fd4646f0e0
JoeriRoijenga <roijenga.joeri@gmail.com>
2021-01-27 11:36:28 +0100

    changes

--- src/app/modules/user/overview/edit-dialog/edit-dialog.component.ts
+++ src/app/modules/user/overview/edit-dialog/edit-dialog.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 import { AuthService } from '../../../../services/auth.service';
 
 interface AdminSelect{
@@ -11,7 +9,7 @@ interface AdminSelect{
 }
 
 @Component({
-  selector: 'app-dialog',
+  selector: 'app-edit-dialog',
   templateUrl: './edit-dialog.component.html',
   styleUrls: ['./edit-dialog.component.scss']
 })
